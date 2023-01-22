61cf8ffb60a7add67e8ae79fdf279eaf73c5c13a
IwanMarcin <marcinm.iwan@gmail.com>
2023-01-22 18:51:44 +0100

    Add new file

---
+++ concateation-of-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function (nums) {
+  let nums2 = [];
+  for (i = 0; i < nums.length; i++) {
+    nums2.push(nums[i]);
+  }
+  let ans = nums.concat(nums2);
+  return ans;
+};
